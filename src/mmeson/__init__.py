dedfb0ba64a4ec45a14cee9000b241d6d71361ae
Stephan Lachnit <stephanlachnit@debian.org>
2023-01-06 16:13:48 +0100

    Set dummy version if not egg distribution is found
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- src/mmeson/__init__.py
+++ src/mmeson/__init__.py
@@ -7,4 +7,8 @@ mmeson is a ccmake clone for Meson projects.
 
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution(__name__).version
+__version__ = 'version-unknown'
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    pass  # running from source folder without installation, run "make" or "setup.py egg_info" to create distribution
